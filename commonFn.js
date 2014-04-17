8a011f415d3ed67fac11912728de8e84c1b0ee28
koala4js <ljc57521@gmail.com>
2014-04-17 14:34:44 +0800

    1

--- commonFn.js
+++ commonFn.js
@@ -231,8 +231,8 @@ function toDouble(num){
 function drag(obj){
 	obj.onmousedown = function(ev){
 		var ev = ev || window.event;
-		var disX = ev.clientX - obj.offsetLeft;
-		var disY = ev.clientY - obj.offsetTop;
+		var disX = ev.clientX - this.offsetLeft;
+		var disY = ev.clientY - this.offsetTop;
 
 		if(obj.setCapture){
 			obj.setCapture();
@@ -240,8 +240,8 @@ function drag(obj){
 
 		document.onmousemove = function(ev){
 			var ev = ev || window.event;
-			obj.style.offsetLeft = ev.clientX - disX + 'px';
-			obj.style.offsetTop = ev.clientY - disY + 'px';
+			obj.style.left = ev.clientX - disX + 'px';
+			obj.style.top = ev.clientY - disY + 'px';
 		}
 
 		document.onmouseup = function(){
